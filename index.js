8c7d3355555756067851776802d03919741acb54
utkarshdubey2008 <97157730+utkarshdubey2008@users.noreply.github.com>
2024-10-18 22:29:50 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>My Game Projects</title>
+</head>
+<body>
+    <h1>Welcome to My Game Projects</h1>
+    <ul>
+        <li><a href="flappy-bird/index.html">Flappy Bird</a></li>
+        <li><a href="game-2/index.html">Game 2</a></li>
+    </ul>
+</body>
+</html>
